1571e026f025a5ff4790cc4312f12e35829b73a9
Daniel Micay <danielmicay@gmail.com>
2019-08-09 16:25:53 -0400

    add more architectures

--- Android.mk
+++ Android.mk
@@ -10,7 +10,7 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
-LOCAL_MODULE_TARGET_ARCH := arm64
+LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/MonochromePublic.apk
 
