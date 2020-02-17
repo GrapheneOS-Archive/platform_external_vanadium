a66d3dde76f9256115fe8a276702a08f650c9957
Daniel Micay <danielmicay@gmail.com>
2020-02-17 04:21:53 -0500

    remove overrides

--- Android.mk
+++ Android.mk
@@ -21,7 +21,6 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support \
         TrichromeLibrary
-LOCAL_OVERRIDES_PACKAGES := webview
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/TrichromeWebView.apk
@@ -35,7 +34,6 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MULTILIB := both
 LOCAL_REQUIRED_MODULES := \
         TrichromeLibrary
-LOCAL_OVERRIDES_PACKAGES := Browser2
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/TrichromeChrome.apk
