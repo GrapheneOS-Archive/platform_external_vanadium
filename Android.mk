e88009754a008f12822bced4522d5abfbf7ec56a
Daniel Micay <danielmicay@gmail.com>
2020-02-16 19:26:26 -0500

    WebView libraries not required for browser

--- Android.mk
+++ Android.mk
@@ -7,9 +7,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_MULTILIB := both
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := \
-        libwebviewchromium_loader \
-        libwebviewchromium_plat_support
 
 LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
